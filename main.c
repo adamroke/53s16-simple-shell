f11abf8b67d0445cabaf33156c618d9058692233
adamroke <Adam@Adams-MacBook-Air.local>
2016-04-20 02:19:13 -0700

    Command struct, parseInput working, quit command working
    both of these methods are now working with the new struct i made to
    store the current command + arguments.
    
    in this stage, each command typed at the prompt prints out a debug
    message that separates the command from the arguments given.
    
    it will parse correctly with multiple spaces, tabs, etc.

--- main.c
+++ main.c
@@ -7,58 +7,91 @@
 #include <string.h>
 #include <stdio.h>
 
-int parseInput(char* input, char *argv[]);  //takes single line of unchanged input in char* input, then outputs on argv, returns 0 if foreground, 1 if background, other values with errors
-int runCommand(int isBackground, char *argv[]); //runs the command contained at argv[0], runs in background if isBackground=1, in foreground if isBackground=0 (& would be stripped from the last arg)
-//returns -1 if "quit" is entered (maybe just do this in parseInput?)
+//struct used for the informaation found in each command call
+struct Command {
+    char command_name[80];
+    char args[40][40];
+    int arg_count;
+};
+typedef struct Command Command;
 
-void cleanup(); //runs at termination of program, should reap all children, perform any other necessary cleanup tasks that won't be done in main
+int parseInput(Command *input, char *init_input); //takes a line of input, parses, and returns status (1 if ok, -1 if error)
+int runCommand(int isBackground, Command *input); //interprets and runs the command, and if necessary forks a new process
+void cleanup(); //reap and kill all children if not already dead
 
-int main()
+
+int main(int argc, char *argv[])
 {
-    int numArgs=40; //80 column display, so max of 40 columns per row if each is one character long w/ one space inbetween
-    int i, isBackground, runStatus;
-    char *input;
-    char *usrArgs[numArgs];
-    input = malloc(sizeof(char)*80);    //allocates string for
-    for(i = 0; i < numArgs; i++){   //allocates strings for arguments
-        usrArgs[i]=malloc(sizeof(char)*80);
-    }
+
+    int parseStatus, runStatus;
+    char *init_input;
+    init_input = malloc(sizeof(char) * 80);
+    Command input;
+
     int exitcheck = 1;
-    while(exitcheck != 0){
-        //Main program loop, will loop indefinitely until user calls quit
-        printf(">");
-        fgets(input,80,stdin);  //obtains 1 line of input
-        isBackground=parseInput(input,usrArgs); //input is parsed, with the return status placed in isBackground
-        if(isBackground == 0 || isBackground == 1){ //if not error state
-            runStatus=(runCommand(isBackground,usrArgs));   //run the command and put run status into runStatus
-            if(runStatus == -1){    //if exit, cleanup and end main loop
+    while(exitcheck != 0) {
+        //Main program loop,
+        printf("> ");
+        fgets(init_input, 80, stdin);  //obtains 1 line of input
+        parseStatus = parseInput(&input, init_input); //input is parsed, with the return status placed in isBackground
+
+        if(parseStatus == 1){ //if not error state
+            runStatus = runCommand(parseStatus, &input); //run the command and put run status into runStatus
+
+            if(runStatus == -1){ //if exit, cleanup and end main loop
                 cleanup();
-                exitcheck=0;
+                exitcheck = 0;
             }
         }
-        else if(isBackground < 0){
-            //if error state when parsing input, handle it (probably not necessary? could just loop back around)
+        else if(parseStatus < 0){
+            printf("Error grabbing input. Retry.\n");
         }
     }
-    free(input);
-    for(i=0; i<numArgs;i++){
-        free(usrArgs[i]);
-    }
+    free(init_input);
+    //FREE THE COMMAND STRUCT
+
     return 0;
 }
 
-int parseInput(char* input, char *argv[]){
-    printf("output would be parsed here, but for now, the input is: %s\n", input);
+int parseInput(Command *input, char *init_input){
+    //takes line of input and fills in the Command struct with the command_name and args[]
+    char *split_input;
+    int count = 0;
+    split_input = strtok(init_input, " \n\t\r");
+    if(split_input != NULL) {
+        strcpy(input->command_name, split_input);
+        split_input = strtok(NULL, " \n\t\r");
+        printf("~~~COMMAND: %s ~~~", input->command_name);
+    } else {
+        return -1;
+    }
+    printf("ARGS: ");
+
+    while(split_input != NULL) {
+        //printf("inpt: %s\n", split_input);
+        strcpy(input->args[count], split_input);
+        split_input = strtok(NULL, " \n\t\r");
+        printf("%s ", input->args[count]);
+        count++;
+        input->arg_count = count;
+    }
+    printf("\n");
+
     return 1;
 }
 
-int runCommand(int isBackground, char *argv[]){
-    printf("command \"%s\" would be run here, with background state of %d\n", argv[0],isBackground);
-    printf("since output hasn't been parsed, there's only gibberish in argv[0]\n");
-    return -1;
+int runCommand(int isBackground, Command *input) {
+    if( strcasecmp(input->command_name, "quit") == 0) {
+        //printf("Exiting...\n");
+        return -1;
+    }
+    //
+    //else if( strcasecmp(..) == 0) { }
+
+    return 0;
 }
 
 void cleanup(){
-    printf("everything would be cleaned up in here, with child processes reaped and such\n");
+    //printf("everything would be cleaned up in here, with child processes reaped and such\n");
     return;
 }
