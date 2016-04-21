ca52c6ba1fd488d54c4c5e20f044b2acec3d8c50
Andrew Light <anlight@uci.edu>
2016-04-21 14:25:52 -0700

    finished runCommand and cleanup, tested and pretty certain they work properly, probably want to test further though

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
+#include <signal.h>
 
 //struct used for the informaation found in each command call
 struct Command {    //but why? you only need the one double array, since command name is supposed to go IN the argument array
@@ -21,6 +22,7 @@ int runCommand(int isBackground, Command *input); //interprets and runs the comm
 void cleanup(); //reap and kill all children if not already dead
 
 int numThreads = 0;
+pid_t pidlist[80]; //assuming a max of 80 processes for now, probably change this
 
 int main(int argc, char *argv[])
 {
@@ -30,16 +32,16 @@ int main(int argc, char *argv[])
     //init_input = malloc(sizeof(char) * 80);
     Command input;
     int i;
-    //for(i = 0; i < 40; i++){
-      //  input.args[i] = malloc(sizeof(char)*80);
-    //}
+    for(i = 0; i < 40; i++){
+        input.args[i] = malloc(sizeof(char)*80);
+    }
     int exitcheck = 1;
     while(exitcheck != 0) {
 	char *init_input;
 	init_input = malloc(sizeof(char)*80);
-	for(i=0; i<40;i++){
-		input.args[i] = malloc(sizeof(char)*80);
-	}
+	//for(i=0; i<40;i++){
+		//input.args[i] = malloc(sizeof(char)*80);
+	//}
 	  //Main program loop,
         printf(">");
         fgets(init_input, 80, stdin);  //obtains 1 line of input
@@ -57,8 +59,9 @@ int main(int argc, char *argv[])
         }
 	free(init_input);
 	for(i=0; i<40; i++){
-		if(input.args[i] == NULL) continue;
-		else free(input.args[i]);//allocate and free them WITHIN the loop, so that assigning a null pointer isn't annoying to deal with
+		if(input.args[i] == NULL){
+			input.args[i] = malloc(sizeof(char)*80);	//should be a little more efficient, only reallocates the NULL termination
+		}
 	}
     }
     //free(init_input);
@@ -92,13 +95,13 @@ int parseInput(Command *input, char *init_input){   //hacked your code a bit, ar
         count++;
         input->arg_count = count;
     }
+    free(input->args[count]);
+	input->args[count] = NULL; //so you need to have the last argument as NULL otherwise it won't run
     background = strcspn(input->args[count-1],"&");
     if(input->args[count-1][background] == '&'){
-        input->args[count-1][background] == '\0';
+        input->args[count-1][background] = '\0';
         return 1;
     }
-	free(input->args[count]);
-	input->args[count] = NULL; //so you need to have the last argument as NULL otherwise it won't run
     printf("\n");
 
     return 0;
@@ -107,6 +110,7 @@ int parseInput(Command *input, char *init_input){   //hacked your code a bit, ar
 int runCommand(int isBackground, Command *input) {
     int pid;
     extern int numThreads;
+    extern pid_t pidlist[];
     if( strcasecmp(input->command_name, "quit") == 0) {
         //printf("Exiting...\n");
         return -1;
@@ -114,23 +118,32 @@ int runCommand(int isBackground, Command *input) {
     else{
         if(isBackground == 1){
             //run in background
-            printf("background task! only it's not implemented lol\n");
+            printf("background entered, with command |%s|\n",input->args[0]);
+            pid = fork();
+		if(pid == 0){
+			execve(input->args[0],input->args);
+			printf("%s\n",strerror(errno));    //prints the error that went wrong with execve (in user use, should only ever return "no such file" and similar errors related to user input
+			exit(0);
+		}
+		else{
+			pidlist[numThreads]=pid;           //parent process just adds pid to array of pids and returns to command prompt
+			numThreads++;
+			//system("ps");                    //uncomment to display list of processes
+		}
+            //put pids from here in array, in cleanup iterate through array, kill and reap each child process
         }
         else{
             //run then wait
-            printf("lol what\n");
             pid = fork();
             if(pid == 0){
-		printf("running program %s\n",input->args[0]);
-        execve(input->args[0],input->args);
-        printf("%s\n",strerror(errno));
+            	execve(input->args[0],input->args);
+            	printf("%s\n",strerror(errno));    //this should only ever run if there's some problem with running the program the user input
                 exit(0);
-        }
-        else{
-            system("ps");
-            wait(pid);
-            numThreads++;   //TODO: work out if this needs to be here, pretty certain threads kill properly when you wait for them
-            printf("main thread here!\n");
+            }
+            else{
+            	//system("ps");
+            	wait(pid);
+            	printf("main thread here!\n");
             }
 
         }
@@ -142,6 +155,18 @@ int runCommand(int isBackground, Command *input) {
 }
 
 void cleanup(){
+	extern int numThreads;
+	extern pid_t pidlist[];
+	int i,childstatus;
+	//system("ps");
+	for(i=0; i<numThreads;i++){  //kills any background processes still running
+		kill(pidlist[i],SIGINT);
+	}
+	for(i=0;i<numThreads;i++){  //loops through all killed background processes, reaping them
+		pid_t wpid = wait(&childstatus);        //note that 
+		if(WIFEXITED(childstatus)) printf("child %d terminated %d\n", wpid, WEXITSTATUS(childstatus));
+		else printf("child terminated abnormally\n");
+	}
     //printf("everything would be cleaned up in here, with child processes reaped and such\n");
     return;
 }
